8611b898e2ab632d761f582dc2c52827b8bbbe9d
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-06-28 23:52:11 +0200

    Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsContainer = document.querySelector('#ingredients');
+
+const makeList = (items) => {
+  return items.map(item => {
+    const newLiEl = document.createElement('li');
+    newLiEl.textContent = item;
+    newLiEl.classList.add('item');
+    return newLiEl;
+  });
+}
+const elements = makeList(ingredients);
+ingredientsContainer.append(...elements);
