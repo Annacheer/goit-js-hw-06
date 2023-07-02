f33bc1387c2226d0b3cf362fa199dd9d4abedbe1
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-07-02 21:14:49 +0200

    Task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const emailInput = form.elements.email;
+  const passwordInput = form.elements.password;
+
+  if (emailInput.value === '' || passwordInput.value === '') {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+
+  form.reset();
+}
 
